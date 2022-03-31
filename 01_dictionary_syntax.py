6ea906f2e024e710243d2f4ec5a1b883cf174dd9
sahil78272026 <sahil.garg78272026@gmail.com>
2022-03-31 05:52:45 +0530

    Add files via upload

---
+++ 01_dictionary_syntax.py
@@ -0,0 +1,19 @@
+# Dictionaries work in key-value pair , they are mutable
+
+myDict = {"First_Name":"Sahil",
+"Last_Name":"Garg",
+"age" : 31,
+"place" : "Ghaziabad",
+"Marriage_Status": False, 
+"Work": {'Airtel':'Network'}
+}
+
+print(myDict)
+print(myDict["First_Name"])
+print(myDict["Last_Name"])
+print(myDict["age"])
+myDict["age"] = 30
+print(myDict)
+print(myDict["Work"]["Airtel"])
+
+
